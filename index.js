267d9c4b6bc07b80f1f4116c70471134e4f3411c
lihuel <l1iperone@gmail.com>
2025-03-30 18:05:54 +0200

    Convert to EJS

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const { logger } = require("./middleware/logger.js");
+const app = express();
+const port = 3000;
 
-// the "./" look in the current folder how to tell node the difference between a module you install and 
+
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use('/', express.static('public'));
+
+
+app.get(/.*bird$/, (req, res) => {
+  res.render("birds", { title: "Information about various birds" });
+});
+
+app.get('/cookies', (req, res) => {
+  console.log(req.query);
+  res.render("cookies", { message: 'Here you soon find all my cookies!' });
+});
+
+app.get("/welcome/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("welcome", { name: name, message: `Welcome, ${name}! Hope you enjoy this simple dynamic backend routing` });
+});
+
+
+
+// Test area
 const data = require("./data.json");
+app.get("/data", (req, res) => {
+  res.render("data", { title: data.title });
+});
 
+app.post('/contact', (req, res) => {
+  console.log('Contact form submission: ', req.body);
+  res.render("contact", { message: 'Thank you for your message. We will be in touch soon.' });
+});
 
-app.use('/', express.static('public'))
-
-  app.get(/.*bird$/, (req, res) => {
-    res.send("Information about various birds");
-  });
-
-
-
-  app.get("/birds/:birdname", (req, res) => {
-    const bird = req.params.birdname;
-    res.send(`Looking at a page about the bird: ${bird}`)
-  })
-
-  app.get("/welcome/:name", (req, res) => {
-    const name = req.params.name;
-    res.send(`Welcome, ${name}! Hope you enjoy this simple dynamic backend routing`);
-  });
-
-  app.get("/data", (req, res) => {
-    res.send (`the product is <strong>${data.title}</strong>`)
-  });
-  
-
-
-
+// Test end
 
 app.listen(port, () => {
-  console.log(`Running on http://localhost:${port}`)
-})
+  console.log(`Running on http://localhost:${port}`);
+});
