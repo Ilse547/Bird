8d41c4a1cf332d2252741ef6f9ee6bf8863a87d8
lihuel <l1iperone@gmail.com>
2025-03-31 11:38:32 +0200

    Add ejs

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const { logger } = require("./middleware/logger.js");
 const app = express();
 const port = 3000;
 
-
+const cookies = [
+  {name: "Chocolate Chip", slug: "chocolate-chip", stock: true},
+  {name: "Banana", slug: "banana", stock: false}
+]
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +15,17 @@ app.set("views", "./pages");
 app.set("view engine", "ejs");
 
 app.get('/', (request, response) => {
-  const numberOfCookiesInStock = 45
-  response.render('index', {numberOfCookiesInStock: numberOfCookiesInStock})
-});
+  const numberOfCookiesInStock = 40
+  response.render('index', {
+    nameOfThePage: "Cookieshop",
+    numberOfCookiesInStock: numberOfCookiesInStock,
+    numberOfCookiesSold: 3283
+  })
+})
 
+app.get('/cookies', (request, response) => {
+  response.render('cookies/index', {cookies:cookies})
+})
 
 app.get("/welcome/:name", (req, res) => {
   const name = req.params.name;
