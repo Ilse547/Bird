851743b4302a93e42a6c19b02df2c1e923fb9dc7
Lihuel <lihuel@debian>
2025-04-14 18:38:50 +0100

    Change database URL

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from './app.js';
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect(MONGODB_URI)
     .then(() => console.log('Database connected'))
     .catch(error => console.error(error))
 export const db = mongoose.connection
